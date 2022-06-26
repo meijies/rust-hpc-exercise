928cef72231bc90ec88fd961c66144c4bb19f362
meijie <meijie@laiye.com>
2022-06-26 22:23:53 +0800

    add sum array with branch benchmark.

--- benches/hpc_benchmark.rs
+++ benches/hpc_benchmark.rs
@@ -1,9 +1,16 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rust_hpc_exercise::fibonacci;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use rust_hpc_exercise::{rand_arry, sum_array_with_branch};
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+fn sum_array_with_branch_benchmark(c: &mut Criterion) {
+    let array = rand_arry();
+    let threshold_array: [u32; 9] = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    let mut group = c.benchmark_group("sum_array_with_branch");
+    for threshold in threshold_array {
+        group.bench_function(BenchmarkId::from_parameter(threshold), |b| {
+            b.iter(|| sum_array_with_branch(black_box(threshold), black_box(array)))
+        });
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, sum_array_with_branch_benchmark);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-#[inline]
-pub fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
+use rand::prelude::*;
+
+pub fn rand_arry() -> [u32; 100] {
+    let mut a: [u32; 100] = [0; 100];
+    let mut rng = thread_rng();
+    for i in 0..99 {
+        a[i] = rng.gen_range(0..100);
     }
+    a
+}
+
+pub fn sum_array_with_branch(threshold: u32, array: [u32; 100]) -> u32 {
+    let mut sum = 0;
+    for item in array {
+        if item < threshold {
+            sum += item;
+        }
+    }
+    sum
 }
